26db3e3f4801ec7049a3897d149ccae1eeeac92c
Felipe <lipecaso@gmail.com>
2023-03-20 12:30:19 +0000

    update wtss request functions

--- R/wtss_request.R
+++ R/wtss_request.R
@@ -18,6 +18,26 @@
     return(response)
 }
 
+#' @title Send a request to WTSS server
+#' @name .wtss_send_request
+#'
+#' @description Sends a request to the WTSS server and times out after 10 tries
+#'
+#' @param request   valid request according to the WTSS protocol
+#' @param ...       additional parameters that can be added in httr.
+#' @return  response from the server
+.wtss_send_request <- function(request, ...) {
+    response <- NULL 
+    ce <- 0
+    # try 5 times (avoid time out connection)
+    while (purrr::is_null(response) & ce < 5) {
+        response <- .wtss_get_response(request, ...)
+        ce <- ce + 1
+    }
+    
+    return(response)
+}
+
 #' @title Get a response to the WTSS server
 #' @name .wtss_get_response 
 #'
@@ -27,12 +47,9 @@
 #' @param ...       additional parameters that can be added in httr.
 #' @return  response from the server
 .wtss_get_response <- function(request, ...) {
-    # check if URL exists and perform the request
-    response <- NULL
-    
-    tryCatch({
-        response <- httr::GET(request, ...)
-        httr::stop_for_status(response)
+     
+    response <- tryCatch({
+        httr::stop_for_status(httr::GET(request, ...))
     }, 
     error = function(e) {
         return(NULL)
@@ -63,6 +80,7 @@
     
     return(json_response)
 }
+
 #' @title Process a request to the WTSS server
 #' @name .wtss_process_request
 #'
@@ -70,8 +88,7 @@
 #'
 #' @param request   valid request to the WTSS service
 #' @return  parsed JSON document
-.wtss_process_request <- function(request){
-    
+.wtss_process_request <- function(request) {
     # avoid time out connection
     result <- .wtss_parse_json(.wtss_send_request(request))
     
