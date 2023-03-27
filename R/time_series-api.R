d1ed7679b4d91997539506b359b589128a6f3f89
Felipe <lipecaso@gmail.com>
2023-03-27 01:01:30 +0000

    add .get_timeseries function

--- R/time_series-api.R
+++ R/time_series-api.R
@@ -1,11 +1,40 @@
 .get_timeseries <- function(URL,
                             name,      
-                            attributes,
                             longitude,
                             latitude, 
+                            attributes,
                             start_date,
                             end_date,
                             token) {
+    
+    # Have we described the coverage before?
+    if (is.null(wtss.env$desc) || .get_name(wtss.env$desc) != name) {
+        wtss.env$desc <- describe_coverage(URL, name)
+        # if describe_coverage fails, return a default time series
+        if (is.null(wtss.env$desc)) {
+            stop(paste("Could not retrieve description of coverage",
+                       name, "from WTSS server."), call. = FALSE)
+        }
+    }
+    
+    # check if the selected attributes are available
+    coverage_bands <- .get_bands(wtss.env$desc)
+    attributes <- .default(attributes, coverage_bands)
+    .check_bands(attributes, coverage_bands)
+    
+    # check bounds for latitude and longitude
+    coverage_bbox <- .get_bbox(wtss.env$desc)
+    .check_bbox(longitude, latitude, coverage_bbox)
+    
+    # Check start and end date
+    timeline <- .get_timeline(wtss.env$desc)
+    start_date <- .default(start_date, min(timeline))
+    end_date <- .default(end_date, max(timeline))
+    
+    # Check start_date and end_date range is valid
+    .check_date(start_date, min = min(timeline), max = max(timeline))
+    .check_date(end_date, min = min(timeline), max = max(timeline))
+    
     # Build url to send request
     req_url <- .build_url(
         url = URL, 
@@ -41,7 +70,7 @@
                               cov_desc = wtss.env$desc)
     
     # Set class         
-    set_class(ts_tbl) <- "wtss"
+    set_class(ts_tbl) <- c("wtss", "sits")
     # Return ...
     return(ts_tbl)
 }
