57ed35d608ab1ddb18807c887849cf0d2f369477
Felipe <lipecaso@gmail.com>
2023-03-31 13:27:19 +0000

    add post function

--- R/url-utils.R
+++ R/url-utils.R
@@ -1,13 +1,7 @@
-#' @title Parse a JSON response from the WTSS server
-#' @name .parse_json
-#'
-#' @description Parse a JSON response from the WTSS service
-#'
-#' @param response a response httr object
-#' @return  parsed JSON document
 .parse_json <- function(response) {
     if (httr::http_type(response) != "application/json") {
-        stop("invalid response type. Expect JSON type...")
+        .error("Invalid response type.",
+               "x" = "Expect JSON document output.")
     }
     parsed_response <- httr::content(
         x = response, 
@@ -22,11 +16,15 @@
     gsub(pattern = "/*$", replacement = "", x = url)
 }
 
+.querystrings_encode <- function(params) {
+  return(lapply(params, paste0, collapse = ","))
+}
+
 .build_url <- function(url, path = NULL, query = NULL, endpoint = NULL) {
     url <- .rm_trailing_dash(url)
     parsed <- httr::parse_url(url)
     parsed[["path"]] <- c(parsed[["path"]], path)
-    parsed[["query"]] <- c(parsed[["query"]], query)
+    parsed[["query"]] <- c(parsed[["query"]], .querystrings_encode(query))
     parsed[["endpoint"]] <- c(parsed[["endpoint"]], endpoint)
     
     return(httr::build_url(parsed))
