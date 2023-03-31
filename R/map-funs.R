62f088c78f3f2158098f0228932faae4eab2de77
Felipe <lipecaso@gmail.com>
2023-03-31 13:25:10 +0000

    add new .map funcitons in map api

--- R/map-funs.R
+++ R/map-funs.R
@@ -2,6 +2,10 @@
     lapply(X = x, FUN = fn, ...)
 }
 
+.map_dbl <- function(x, fn, ...) {
+    vapply(X = x, FUN = fn, ..., FUN.VALUE = numeric(1))
+}
+
 .map_chr <- function(x, fn, ...) {
     vapply(X = x, FUN = fn, ..., FUN.VALUE = character(1))
 }
@@ -17,4 +21,21 @@
         stop("'fn' should return a tibble in .map_dfr")
     }
     return(do.call(rbind, result_lst))
+}
+
+.map_dfc <- function(x, fn, ...) {
+    result_lst <- lapply(X = x, FUN = fn, ...)
+    is_elem_tbl <- .map_lgl(result_lst, .is_tbl)
+    if (!any(is_elem_tbl)) {
+        stop("'fn' should return a tibble in .map_dfc")
+    }
+    return(do.call(cbind, result_lst))
+}
+
+.rbind <- function(x, ...) {
+    do.call(rbind, x, ...)
+}
+
+.cbind <- function(x, ...) {
+    do.call(cbind, x, ...)
 }
